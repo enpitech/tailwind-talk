069b34d7091fdd8821fd50505b79bb53f6263117
Nir Parisian <nir.parisian@gmail.com>
2023-07-07 14:48:31 +0300

    fun

--- src/slides/Slide2.jsx
+++ src/slides/Slide2.jsx
@@ -1,7 +1,9 @@
 export default function Slide2() {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-white shadow-md">
-      <h1 className="text-9xl font-bold">Important Benefits of Tailwind CSS</h1>
+      <h1 className="text-9xl font-bold text-red-100 text-left mt-2 flex">
+        Important Benefits of Tailwind CSS
+      </h1>
     </div>
-  );
+  )
 }
